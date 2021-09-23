b757a1f8f5d44192ac9042ad21fe53101bb008aa
Alissa <alissayarmantho1@gmail.com>
2021-09-24 00:22:16 +0800

    Fix comments

--- src/components/Text/Text.stories.tsx
+++ src/components/Text/Text.stories.tsx
@@ -4,7 +4,7 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Text from "./Text";
 
 export default {
-  title: "Component/Text",
+  title: "Text",
   component: Text,
   argTypes: {
     color: { control: "color" },
@@ -13,47 +13,19 @@ export default {
 
 const Template: ComponentStory<typeof Text> = (args) => <Text {...args} />;
 
-export const Header = Template.bind({});
-Header.args = {
-  variant: "header",
+export const Default = Template.bind({});
+
+export const StrikeThrough = Template.bind({});
+StrikeThrough.args = {
+  decoration: "line-through",
 };
 
-export const SubHeader = Template.bind({});
-SubHeader.args = {
-  variant: "subheader",
+export const UnderLine = Template.bind({});
+UnderLine.args = {
+  decoration: "underline",
 };
 
-export const Body = Template.bind({});
-Body.args = {
-  variant: "body",
-};
-
-export const BodySmall = Template.bind({});
-BodySmall.args = {
-  variant: "body-small",
-};
-
-export const SubText = Template.bind({});
-SubText.args = {
-  variant: "subtext",
-};
-
-export const HeaderAlt = Template.bind({});
-HeaderAlt.args = {
-  variant: "header-alt",
-};
-
-export const SubHeaderAlt = Template.bind({});
-SubHeaderAlt.args = {
-  variant: "subheader-alt",
-};
-
-export const BodyAlt = Template.bind({});
-BodyAlt.args = {
-  variant: "body-alt",
-};
-
-export const SubTextAlt = Template.bind({});
-SubTextAlt.args = {
-  variant: "subtext-alt",
+export const Italics = Template.bind({});
+Italics.args = {
+  fontStyle: "italic",
 };
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -3,62 +3,70 @@ import "./text.css";
 
 export interface TextProps {
   /**
-   * Text variants
+   * Text font sizes
    */
-  variant?:
-    | "header"
-    | "subheader"
-    | "body"
-    | "body-small"
-    | "subtext"
-    | "header-alt"
-    | "subheader-alt"
-    | "body-alt"
-    | "subtext-alt";
+  fontSize?:
+    | "xs"
+    | "sm"
+    | "base"
+    | "lg"
+    | "xl"
+    | "2xl"
+    | "3xl"
+    | "4xl"
+    | "5xl"
+    | "6xl"
+    | "7xl"
+    | "8xl"
+    | "9xl";
+  /**
+   * Text font weights
+   */
+  fontWeight?:
+    | "thin"
+    | "extralight"
+    | "light"
+    | "normal"
+    | "medium"
+    | "semibold"
+    | "bold"
+    | "extrabold"
+    | "black";
+  /**
+   * Overflow style eg: truncate with ellipsis or clip
+   */
+  overflow?: "truncate" | "clip";
+  /**
+   * Text decoration eg: underline, line-through
+   */
+  decoration?: "underline" | "line-through";
   /**
    * Text color to use (accepts hex, eg: #212121 or #FF0000)
    */
   color?: string;
   /**
-   * Font style of the text eg: italics, etc
+   * Font style of the text eg: italics or normal
    */
-  fontStyle?: string;
+  fontStyle?: "italic" | "normal";
   /**
    * Text to be displayed
    */
   text?: string;
 }
-const textVariantStyles = {
-  header: { size: "3rem", weight: 700, lineHeight: 1 },
-  subheader: { size: "1.5rem", weight: 700, lineHeight: "2rem" },
-  body: { size: "1.25rem", weight: 400, lineHeight: "1.75rem" },
-  "body-small": { size: "1.125rem", weight: 400, lineHeight: "1.75rem" },
-  subtext: { size: "1rem", weight: 400, lineHeight: "1.5rem" },
-  "header-alt": { size: "2.25rem", weight: 800, lineHeight: "2.5rem" },
-  "subheader-alt": { size: "1.25rem", weight: 400, lineHeight: "1.75rem" },
-  "body-alt": { size: "1rem", weight: 400, lineHeight: "1.5rem" },
-  "subtext-alt": { size: "1.25rem", weight: 700, lineHeight: "1.75rem" },
-};
 
 const Text: React.FC<TextProps> = ({
-  variant = "body",
+  fontSize = "base",
+  fontWeight = "normal",
+  overflow,
+  decoration,
   color = "#212121",
   fontStyle = "normal",
   text,
 }) => {
-  const variantStyle = textVariantStyles[variant];
-  const { size, weight, lineHeight } = variantStyle;
-
   return (
     <p
-      style={{
-        color: color,
-        fontStyle: fontStyle,
-        fontSize: size,
-        fontWeight: weight,
-        lineHeight: lineHeight,
-      }}
-      className="text"
+      style={{ color: color }}
+      className={`text text--${fontSize} text--${fontWeight} text--${overflow} text--${decoration} text--${fontStyle}`}
     >
       {text}
     </p>
