3d7da318b6e7fcbffc443d1a4301f9c5d010a795
simonjulianl <simonjulian21@gmail.com>
2021-09-24 14:15:18 +0800

    Add header for desktop

--- src/components/Header/Header.stories.tsx
+++ src/components/Header/Header.stories.tsx
@@ -1,19 +1,63 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
+import "../global.styles.css";
 import Header from "./Header";
 
 export default {
-  title: "Example/Header",
+  title: "Header",
   component: Header,
 } as ComponentMeta<typeof Header>;
 
 const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
 
+const logoPath =
+  "https://images.fastcompany.net/image/upload/w_596,c_limit,q_auto:best,f_auto/fc/3034007-inline-i-applelogo.jpg";
+
+const title = "PINUS";
+const headers = [
+  {
+    label: "home",
+    url: "https://google.com",
+  },
+  {
+    label: "events",
+    url: "https://google.com",
+  },
+  {
+    label: "about us",
+    url: "https://google.com",
+  },
+  {
+    label: "another header",
+    url: "https://google.com",
+  },
+  {
+    label: "very very long header",
+    url: "https://google.com",
+  },
+];
+
+export const Vanilla = Template.bind({});
+Vanilla.args = {
+  logoPath: logoPath,
+  headerTitle: title,
+  headers: headers,
+};
+
 export const LoggedIn = Template.bind({});
 LoggedIn.args = {
+  logoPath: logoPath,
+  headerTitle: title,
+  headers: headers,
   user: {},
+  isLoginSupported: true,
 };
 
 export const LoggedOut = Template.bind({});
-LoggedOut.args = {};
+LoggedOut.args = {
+  logoPath: logoPath,
+  headerTitle: title,
+  headers: headers,
+  isLoginSupported: true,
+};
